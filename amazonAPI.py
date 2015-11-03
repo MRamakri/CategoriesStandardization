6e498b2971693ecf68c36dd1d93973b681a35258
MRamakri <manikandan.ramakrishnan@inmar.com>
2015-11-03 00:58:15 -0500

    Added asin_description_lookup method to get product description

--- amazonAPI.py
+++ amazonAPI.py
@@ -1,4 +1,5 @@
 import bottlenose
+from bs4 import BeautifulSoup
 
 access_key = "AKIAICAETJ4OV7TWQKKQ"
 secret_key = "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"
@@ -16,7 +17,21 @@ def upc_category_lookup(upc):
     response = amazon.ItemLookup(ItemId=upc, IdType="UPC", SearchIndex="All", ResponseGroup="BrowseNodes")
     return response
 
+def asin_description_lookup(asin):
+    """
+    Looks up the given ASIN in the Amazon database and returns the descripiton of the item
+    :param asin: ASIN of the item
+    :return: response: Query response provided by Amazon
+    """
+    response = amazon.ItemLookup(ItemId=asin, IdType="ASIN", ResponseGroup="ItemAttributes")
+    soup = BeautifulSoup(response, "html.parser")
+    description = soup.find('title').text
+    return description
+
 if __name__ == "__main__":
-    upc = "021000018604"
+    upc = "038000324109"
+    asin = "B00CHU3XW8"
     response = upc_category_lookup(upc)
+    respons2 = asin_description_lookup(asin)
     print(response)
+    print(respons2)
